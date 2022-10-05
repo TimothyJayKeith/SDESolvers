a90dc9b77e7616cdabfbbb1168f3446f2c66e23d
Tim Keith <timothykeith67@gmail.com>
2022-10-04 22:47:00 -0600

    My first attempt at creating a function that approximates the Ito process using the Euler method.

--- ito_process.py
+++ ito_process.py
@@ -1,6 +1,28 @@
 import numpy as np
-import scipy as sp
 
 def ito_approx(a, b, init_val=0, T=1, N=1):
-    # Delta = T/N
-    pass
+    """
+    Approximates the ito process using the Euler approximation
+
+    Parameters
+    ----------
+    a : drift
+    b : diffusion
+    init_val : intial condition
+    T : length of time interval
+    N : number of time steps
+    
+    Return
+    ------
+    val_dict: dictionary of values at each time step
+    """
+    Delta = T/N
+    Y = init_val
+    val_dict = {0: Y}
+    for n in range(N):
+        Y += a(Y)*Delta + b(Y)*np.random.normal(0, Delta)
+        val_dict[(n+1)*Delta] = Y
+    return val_dict
+
+if __name__ == "__main__":
+    print(ito_approx(lambda x : 1, lambda x : x, N=5))
