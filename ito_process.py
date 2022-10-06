73842ff40d9416dff2f4f79bbf8736a764d21169
Tim Keith <timothykeith67@gmail.com>
2022-10-05 19:46:08 -0600

    Added documentation and parameters to ito_process
    New parameter called return_full_sim. If set to true, it will return a dictionary
    detailing the entire process.

--- ito_process.py
+++ ito_process.py
@@ -1,20 +1,34 @@
 import numpy as np
 
-def ito_approx(a, b, init_val=0, T=1, N=1):
+def ito_approx(a, b, init_val=0, T=1, N=1, return_full_sim=False):
     """
     Approximates the ito process using the Euler approximation
 
     Parameters
     ----------
-    a : drift
-    b : diffusion
-    init_val : intial condition
-    T : length of time interval
-    N : number of time steps
+    a : lambda
+        The "drift" of the process, or the deterministic portion
+    b : lambda
+        The "diffusion" of the process, or the random portion
+    init_val : float
+        The initial condition of the process
+    T : positive double
+        The length of the time interval (time is assumed to always
+        start at 0)
+    N : positive int
+        The number of time steps
+    return_full_sim : bool
+        If set to True, will return a dictionary with the value of
+        the process at each time step. Otherwise only returns the
+        final value of the process
     
-    Return
-    ------
-    val_dict: dictionary of values at each time step
+    Returns
+    -------
+    val_dict: dictionary
+        Keys are the time steps, values are the values at the time
+        step. Only returns if return_full_sim=True
+    Y: float
+        Value of the process at the final time step
     """
     Delta = T/N
     Y = init_val
@@ -22,4 +36,7 @@ def ito_approx(a, b, init_val=0, T=1, N=1):
     for n in range(N):
         Y += a(Y)*Delta + b(Y)*np.random.normal(0, Delta)
         val_dict[(n+1)*Delta] = Y
-    return val_dict
+    if return_full_sim:
+        return val_dict
+    else:
+        return Y
